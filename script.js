00241fc0d235bab763b9e3bd4cd905315d8c2d6f
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-07 16:13:24 -0300

    created load function, adjust loader/finger displays

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const progressBar = document.querySelector('.progress-bar');
 const progressBarLoaded = document.querySelector('.bar-loaded');
 const totalFrames = 24;
 let loadedFrames = 0;
+const finger = document.querySelector('.finger-icon');
 
 const load = () => {
+  finger.style.display = 'none';
   for (i = 0; i < 24; i++) {
     const newImg = document.createElement('img');
     newImg.classList.add('pic');
@@ -29,43 +31,44 @@ const load = () => {
       progressBarLoaded.style.width = `${(loadedFrames / totalFrames) * 100}%`;
       console.log(loadedFrames);
       if (loadedFrames === totalFrames) {
+        finger.style.display = 'block';
         progressBar.style.zIndex = '0';
         progressBar.style.display = 'none';
         progressBar.style.opacity = 0;
         progressBarLoaded.style.display = 'none';
         progressBarLoaded.style.opacity = 0;
+
+        //Events:
+        const pics = document.querySelectorAll('.pic');
+        pics[0].style.opacity = 1;
+
+        pics[0].style.zIndex = '10';
+        pics[0].addEventListener('pointermove', (e) => {
+          finger.style.display = 'none';
+          const x = e.offsetX;
+          // console.log(x);
+          const cWidth = pics[0].offsetWidth;
+          const l = pics.length;
+          pics.forEach((pic, i, arr) => {
+            pic.classList.remove('on');
+            if (x >= (cWidth / l) * i && x < (cWidth / l) * (i + 1)) {
+              pic.style.opacity = 1;
+              pic.classList.add('on');
+              arr.forEach((p) => {
+                if (!p.classList.contains('on')) {
+                  p.style.opacity = 0;
+                }
+              });
+            }
+          });
+        });
+
+        pics[0].addEventListener('pointerleave', () => {
+          finger.style.display = 'block';
+        });
       }
     };
   }
-
-  const pics = document.querySelectorAll('.pic');
-  pics[0].style.opacity = 1;
-  const finger = document.querySelector('.finger-icon');
-
-  pics[0].style.zIndex = '10';
-  pics[0].addEventListener('pointermove', (e) => {
-    finger.style.display = 'none';
-    const x = e.offsetX;
-    // console.log(x);
-    const cWidth = pics[0].offsetWidth;
-    const l = pics.length;
-    pics.forEach((pic, i, arr) => {
-      pic.classList.remove('on');
-      if (x >= (cWidth / l) * i && x < (cWidth / l) * (i + 1)) {
-        pic.style.opacity = 1;
-        pic.classList.add('on');
-        arr.forEach((p) => {
-          if (!p.classList.contains('on')) {
-            p.style.opacity = 0;
-          }
-        });
-      }
-    });
-  });
-
-  pics[0].addEventListener('pointerleave', () => {
-    finger.style.display = 'block';
-  });
 };
 
 load();
