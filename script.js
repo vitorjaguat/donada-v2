efb6b980b318d04db7b1f5643ab99553c8c39839
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-07 15:01:31 -0300

    refactor code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const body = document.body;
 const container = document.querySelector('.container');
 
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
 for (i = 0; i < 24; i++) {
   const newImg = document.createElement('img');
   newImg.classList.add('pic');
@@ -17,15 +21,44 @@ for (i = 0; i < 24; i++) {
 }
 
 const pics = document.querySelectorAll('.pic');
-
-function scale(number, inMin, inMax, outMin, outMax) {
-  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-}
-
 pics[0].style.opacity = 1;
-let activeIndex = 0;
 const finger = document.querySelector('.finger-icon');
 
+//load images and prepare events:
+
+const spinner = document.querySelector('.loader-img');
+const progress = document.querySelector('.progress');
+const totalFrames = 24;
+let loaded = 0;
+
+const load = () => {};
+
+pics[0].style.zIndex = '1000';
+pics[0].addEventListener('pointermove', (e) => {
+  finger.style.display = 'none';
+  const x = e.offsetX;
+  // console.log(x);
+  const cWidth = pics[0].offsetWidth;
+  const l = pics.length;
+  pics.forEach((pic, i, arr) => {
+    pic.classList.remove('on');
+    if (x >= (cWidth / l) * i && x < (cWidth / l) * (i + 1)) {
+      pic.style.opacity = 1;
+      pic.classList.add('on');
+      arr.forEach((p) => {
+        if (!p.classList.contains('on')) {
+          p.style.opacity = 0;
+        }
+      });
+    }
+  });
+});
+
+pics[0].addEventListener('pointerleave', () => {
+  finger.style.display = 'block';
+});
+
+//OTHER EVENTS:
 //Events:
 // pics[0].addEventListener('mouseenter', (e) => {
 //   const cWidth = pics[0].offsetWidth;
@@ -87,27 +120,3 @@ const finger = document.querySelector('.finger-icon');
 //     }
 //   });
 // });
-pics[0].style.zIndex = '1000';
-pics[0].addEventListener('pointermove', (e) => {
-  finger.style.display = 'none';
-  const x = e.offsetX;
-  // console.log(x);
-  const cWidth = pics[0].offsetWidth;
-  const l = pics.length;
-  pics.forEach((pic, i, arr) => {
-    pic.classList.remove('on');
-    if (x >= (cWidth / l) * i && x < (cWidth / l) * (i + 1)) {
-      pic.style.opacity = 1;
-      pic.classList.add('on');
-      arr.forEach((p) => {
-        if (!p.classList.contains('on')) {
-          p.style.opacity = 0;
-        }
-      });
-    }
-  });
-});
-
-pics[0].addEventListener('pointerleave', () => {
-  finger.style.display = 'block';
-});
